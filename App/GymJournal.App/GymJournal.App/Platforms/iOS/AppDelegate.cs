df3b5481542facd70a922abb98e6de344feb57ec
marian222200 <marian222200@gmail.com>
2023-06-05 04:38:43 +0300

    Added MAUI App
    Separated repo in API and App (UI)

---
+++ App/GymJournal.App/GymJournal.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace GymJournal.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
