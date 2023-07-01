4cd4c675fa518961ddcf60f901fcbf5f1b3efe8c
marian222200 <marian222200@gmail.com>
2023-07-01 18:53:02 +0300

    now upon logging in on another device, the app signs you out

--- GymJournal.App/GymJournal.App/Services/ExceptionHandlerService.cs
+++ GymJournal.App/GymJournal.App/Services/ExceptionHandlerService.cs
@@ -1,19 +1,29 @@
 ﻿using GymJournal.App.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using GymJournal.App.View;
 
 namespace GymJournal.App.Services
 {
 	public class ExceptionHandlerService
 	{
+		private readonly IdentityService _identityService;
+		public ExceptionHandlerService(IdentityService identityService)
+		{
+			_identityService = identityService ?? throw new ArgumentNullException(nameof(identityService));
+		}
 		public async Task HandleException(Exception ex)
 		{
 
 			if (ex is ServerRequestException)
-				await Shell.Current.DisplayAlert("Server Request Error", ex.Message, "OK");
+			{
+				if (ex.Message.Contains("invalid UserToken"))
+				{
+					_identityService.DeleteIdentity();
+					await Shell.Current.DisplayAlert("Signed Out", "You have been signed out.", "OK");
+					await Shell.Current.GoToAsync($"//{nameof(MainPage)}", true);
+				}
+				else
+					await Shell.Current.DisplayAlert("Server Request Error", ex.Message, "OK");
+			}
 			else
 			{
 				await Shell.Current.DisplayAlert("Error", ex.Message, "OK");
